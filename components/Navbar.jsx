e213210f1f0313c4dbdc4e6eac6895edb2bcd040
araaancode <araaancode@gmail.com>
2024-03-14 16:26:05 +0330

    first commit

---
+++ components/Navbar.jsx
@@ -0,0 +1,38 @@
+import Link from "next/link";
+import { useSelector } from "react-redux";
+import styles from "../styles/Navbar.module.css";
+
+const Navbar = () => {
+  const cart = useSelector((state) => state.cart);
+
+  const getItemsCount = () => {
+    return cart.reduce((accumulator, item) => accumulator + item.quantity, 0);
+  };
+
+  return (
+    <nav className={styles.navbar}>
+      <ul className={styles.links}>
+        <li className={styles.navlink}>
+          <Link href="/cart">
+            <a>سبد خرید ({getItemsCount()})</a>
+          </Link>
+        </li>
+
+        <li className={styles.navlink}>
+          <Link href="/about">درباره</Link>
+        </li>
+        <li className={styles.navlink}>
+          <Link href="/contact">تماس</Link>
+        </li>
+        <li className={styles.navlink}>
+          <Link href="/shop">محصولات</Link>
+        </li>
+        <li className={styles.navlink}>
+          <Link href="/">خانه</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
